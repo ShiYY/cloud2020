9e3fa0fa1aa20662cfbbdd0c339121cefd29ad89
shiyang <2209692570@qq.com>
2020-03-12 22:53:08 +0800

    Add init module cloud-consumer-order80

---
+++ cloud-consumer-order80/src/main/java/com.shiyang.springcloud/OrderApplication.java
@@ -0,0 +1,8 @@
+package com.shiyang.springcloud;
+
+/**
+ * @author shiyang
+ * @create 2020-03-12 9:26 下午
+ */
+public class OrderApplication {
+}
--- cloud-provider-payment8001/src/main/java/com/shiyang/springcloud/PaymentApplication.java
+++ cloud-provider-payment8001/src/main/java/com/shiyang/springcloud/PaymentApplication.java
@@ -4,5 +4,6 @@ package com.shiyang.springcloud;
  * @author shiyang
  * @create 2020-03-11 11:42 下午
  */
+
 public class PaymentApplication {
 }
