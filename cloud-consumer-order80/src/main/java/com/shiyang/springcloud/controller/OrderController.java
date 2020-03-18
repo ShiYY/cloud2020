9f98879e451f3ca8fa8147b8abd6e34aebe6d8ee
ShiYY <2209692570@qq.com>
2020-03-18 11:37:44 +0800

    Add - RestTemplate.getForEntity

--- cloud-consumer-order80/src/main/java/com/shiyang/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/shiyang/springcloud/controller/OrderController.java
@@ -1,6 +1,9 @@
 package com.shiyang.springcloud.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,8 @@ import com.shiyang.springcloud.entities.Payment;
  */
 @RestController
 public class OrderController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
     private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Autowired
@@ -28,5 +33,23 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity =
+            restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            logger.info(entity.getStatusCode().toString());
+            return entity.getBody();
+        } else {
+            return new CommonResult(444, "操作失败", null);
+        }
+    }
+
+    @GetMapping("/consumer/payment/createForEntity")
+    public CommonResult<Payment> create2(Payment payment) {
+        return restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class).getBody();
     }
 }
